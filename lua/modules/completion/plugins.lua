0abefc51597b5f645a5f15ff5459df4e06cc592e
shi-lai-mu <478889187@qq.com>
2022-07-27 21:09:34 +0800

    feat: 方案二

---
+++ lua/modules/completion/plugins.lua
@@ -0,0 +1,68 @@
+local completion = {}
+local conf = require("modules.completion.config")
+
+completion["neovim/nvim-lspconfig"] = {
+	opt = true,
+	event = "BufReadPre",
+	config = conf.nvim_lsp,
+}
+completion["creativenull/efmls-configs-nvim"] = {
+	opt = false,
+	requires = "neovim/nvim-lspconfig",
+}
+completion["williamboman/nvim-lsp-installer"] = {
+	opt = false,
+}
+completion["kevinhwang91/nvim-bqf"] = {
+	opt = true,
+	ft = "qf",
+	config = conf.bqf,
+}
+completion["tami5/lspsaga.nvim"] = {
+	opt = true,
+	after = "nvim-lspconfig",
+}
+completion["stevearc/aerial.nvim"] = {
+	opt = true,
+	after = "nvim-lspconfig",
+	config = conf.aerial,
+}
+completion["kosayoda/nvim-lightbulb"] = {
+	opt = true,
+	after = "nvim-lspconfig",
+	config = conf.lightbulb,
+}
+completion["ray-x/lsp_signature.nvim"] = { opt = true, after = "nvim-lspconfig" }
+completion["hrsh7th/nvim-cmp"] = {
+	config = conf.cmp,
+	event = "InsertEnter",
+	requires = {
+		{ "lukas-reineke/cmp-under-comparator" },
+		{ "saadparwaiz1/cmp_luasnip", after = "LuaSnip" },
+		{ "hrsh7th/cmp-nvim-lsp", after = "cmp_luasnip" }, --neovim 内置 LSP 客户端的 nvim-cmp 源
+		{ "hrsh7th/cmp-nvim-lua", after = "cmp-nvim-lsp" },, --nvim-cmp source for neovim Lua API.
+		{ "andersevenrud/cmp-tmux", after = "cmp-nvim-lua" },
+		{ "hrsh7th/cmp-path", after = "cmp-tmux" },
+		{ "f3fora/cmp-spell", after = "cmp-path" },--nvim-cmp 的拼写源基于 vim 的拼写建议
+		{ "hrsh7th/cmp-buffer", after = "cmp-spell" }, --从buffer中智能提示
+		{ "kdheepak/cmp-latex-symbols", after = "cmp-buffer" },
+		-- {
+		-- 	"tzachar/cmp-tabnine",
+		-- 	run = "./install.sh",
+		-- 	after = "cmp-latex-symbols",
+		-- 	config = conf.tabnine,
+		-- },
+	},
+}
+completion["L3MON4D3/LuaSnip"] = {
+	after = "nvim-cmp",
+	config = conf.luasnip,
+	requires = "rafamadriz/friendly-snippets", --代码段合集
+}
+completion["windwp/nvim-autopairs"] = {
+	after = "nvim-cmp",
+	config = conf.autopairs,
+}
+completion["github/copilot.vim"] = { opt = true, cmd = "Copilot" }
+
+return completion
---
+++ lua/modules/editor/plugins.lua
@@ -0,0 +1,135 @@
+local editor = {}
+local conf = require("modules.editor.config")
+
+editor["junegunn/vim-easy-align"] = { opt = true, cmd = "EasyAlign" }
+editor["RRethy/vim-illuminate"] = {
+	event = "BufReadPost",
+	config = conf.illuminate,
+}
+editor["terrortylor/nvim-comment"] = {
+	opt = false,
+	config = conf.nvim_comment,
+}
+editor["nvim-treesitter/nvim-treesitter"] = {
+	opt = true,
+	run = ":TSUpdate",
+	event = "BufReadPost",
+	config = conf.nvim_treesitter,
+}
+editor["nvim-treesitter/nvim-treesitter-textobjects"] = {
+	opt = true,
+	after = "nvim-treesitter",
+}
+editor["p00f/nvim-ts-rainbow"] = {
+	opt = true,
+	after = "nvim-treesitter",
+	event = "BufReadPost",
+}
+editor["JoosepAlviste/nvim-ts-context-commentstring"] = {
+	opt = true,
+	after = "nvim-treesitter",
+}
+editor["mfussenegger/nvim-ts-hint-textobject"] = {
+	opt = true,
+	after = "nvim-treesitter",
+}
+editor["windwp/nvim-ts-autotag"] = {
+	opt = true,
+	after = "nvim-treesitter",
+	config = conf.autotag,
+}
+editor["andymass/vim-matchup"] = {
+	opt = true,
+	after = "nvim-treesitter",
+	config = conf.matchup,
+}
+editor["rhysd/accelerated-jk"] = { opt = true, event = "BufWinEnter" }
+editor["hrsh7th/vim-eft"] = { opt = true, event = "BufReadPost" }
+editor["romainl/vim-cool"] = {
+	opt = true,
+	event = { "CursorMoved", "InsertEnter" },
+}
+editor["phaazon/hop.nvim"] = {
+	opt = true,
+	branch = "v2",
+	event = "BufReadPost",
+	config = conf.hop,
+}
+editor["karb94/neoscroll.nvim"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = conf.neoscroll,
+}
+editor["vimlab/split-term.vim"] = { opt = true, cmd = { "Term", "VTerm" } }
+editor["akinsho/toggleterm.nvim"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = conf.toggleterm,
+}
+editor["numtostr/FTerm.nvim"] = { opt = true, event = "BufReadPost" }
+editor["norcalli/nvim-colorizer.lua"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = conf.nvim_colorizer,
+}
+editor["rmagatti/auto-session"] = {
+	opt = true,
+	cmd = { "SaveSession", "RestoreSession", "DeleteSession" },
+	config = conf.auto_session,
+}
+editor["max397574/better-escape.nvim"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = conf.better_escape,
+}
+editor["rcarriga/nvim-dap-ui"] = {
+	opt = false,
+	config = conf.dapui,
+	requires = {
+		{ "mfussenegger/nvim-dap", config = conf.dap },
+		{
+			"Pocco81/dap-buddy.nvim",
+			opt = true,
+			cmd = { "DIInstall", "DIUninstall", "DIList" },
+			commit = "24923c3819a450a772bb8f675926d530e829665f",
+			config = conf.dapinstall,
+		},
+	},
+}
+editor["tpope/vim-fugitive"] = { opt = true, cmd = { "Git", "G" } }
+editor["famiu/bufdelete.nvim"] = {
+	opt = true,
+	cmd = { "Bdelete", "Bwipeout", "Bdelete!", "Bwipeout!" },
+}
+editor["edluffy/specs.nvim"] = {
+	opt = true,
+	event = "CursorMoved",
+	config = conf.specs,
+}
+editor["abecodes/tabout.nvim"] = {
+	opt = true,
+	event = "InsertEnter",
+	wants = "nvim-treesitter",
+	after = "nvim-cmp",
+	config = conf.tabout,
+}
+editor["sindrets/diffview.nvim"] = {
+	opt = true,
+	cmd = { "DiffviewOpen" },
+}
+editor["brglng/vim-im-select"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = conf.imselect,
+}
+editor["luukvbaal/stabilize.nvim"] = {
+	opt = true,
+	event = "BufReadPost",
+}
+editor["airblade/vim-rooter"] = {
+    opt = true,
+    event = "BufWinEnter",
+    config = conf.vim_roiter,    
+}
+
+return editor
